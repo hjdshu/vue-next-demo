499055da07869af2868f8deefd4790f915012e20
hejia <jia.he@yottacloud.cn>
2020-07-13 15:56:51 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,34 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>x:{{x}}, y: {{y}}</p>
+    <button @click="addhello">addhello</button>
+    <HelloWorld v-if="hello" msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import useMousePosition from './hooks/useMousePosition'
+import { ref } from 'vue'
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  setup () {
+    let {x, y} = useMousePosition()
+    let hello = ref(false)
+    function addhello () {
+      hello.value = true
+    }
+    return {
+      hello,
+      addhello,
+      x,
+      y
+    }
   }
 }
 </script>
---
+++ src/hooks/useMousePosition.js
@@ -0,0 +1,21 @@
+import { ref, onMounted, onUnmounted } from 'vue'
+
+export default function useMousePositon () {
+  let x = ref(0)
+  let y = ref(0)
+
+  let listenmove = function (e) {
+    x.value = e.pageX
+    y.value = e.pageY
+  }
+
+  onMounted(() => {
+    window.addEventListener('mousemove', listenmove)
+    console.log('mousemove init')
+  })
+  onUnmounted(() => {
+    window.removeEventListener('mousemove', listenmove)
+  })
+
+  return {x, y}
+}
